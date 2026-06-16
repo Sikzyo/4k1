b792a3ea8e2a4c6e124d701b810417a29a04aba4
Jane <183669693+Sikzyo@users.noreply.github.com>
2026-06-16 08:51:16 -0500

    feat: add macOS entry point installer script

---
+++ install_mac.sh
@@ -0,0 +1,44 @@
+echo "✦ Inicio de configuración para MacOs"
+set -e
+
+if ! xcode-select -p &> /dev/null; then
+    echo ""
+    echo "✦ Instalando Xcode Command Line Tools" 
+
+    xcode-select --install
+
+    echo ""
+    echo "✦ Se ha abierto una ventana del sistema macOS."
+    echo "✦ Ve a esa ventana y completa la instalación."
+    echo "✦ Cuando la instalación haya terminado, regresa aquí."
+
+    while pgrep -f "Install Command Line Developer Tools" &> /dev/null; do
+        sleep 5
+    done
+
+    echo ""
+    echo "✦ ¡Linea de comandos instalada correctamente!"
+fi
+
+DOTFILE_DIR=~/.dotfile
+
+if [ ! -d "$DOTFILE_DIR" ]; then
+    echo ""
+    echo "✦ Clonando repositorio"
+    git clone https://github.com/Sikzyo/4k1.git "$DOTFILE_DIR"
+else
+    echo ""
+    echo "✦ El repositorio ya existe, actualizando..."
+    cd "$DOTFILE_DIR"
+    git pull origin main
+fi
+
+cd "$DOTFILE_DIR"
+if [ -f "mac/install.sh" ]; then
+    echo ""
+    echo "✦ Iniciando instalador de Mac"
+    bash mac/install.sh
+else
+    echo "Error al iniciar el instalador"
+    exit 1
+fi
