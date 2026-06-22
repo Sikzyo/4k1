0bb53be4566eac828acf815b3fd51ccee1642c2c
jane <183669693+Sikzyo@users.noreply.github.com>
2026-06-22 11:19:22 -0500

    chore: add homebrew installer

---
+++ mac/scripts/modules/01_homebrew.sh
@@ -0,0 +1,47 @@
+echo "✦ Modulo de Homebrew ✦"
+set -e
+
+sleep 0
+
+echo ""
+echo "=> Validando si esta instalado Homebrew"
+
+sleep 0
+
+if command -v brew &> /dev/null; then
+    echo ""
+    echo "✦ Homebrew ya se encuentra instalado"
+else
+    echo ""
+    echo "✦ Iniciando descarga de Homebrew"
+
+    sleep 0
+
+    echo ""
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    sleep 0
+
+    echo ""
+    if ! grep -q "brew shellenv" "$HOME/.zprofile"; then
+        echo >> "$HOME/.zprofile"
+        echo 'eval "$(/opt/homebrew/bin/brew shellenv zsh)"' >> "$HOME/.zprofile"
+    fi
+    eval "$(/opt/homebrew/bin/brew shellenv zsh)"
+
+    sleep 0
+
+    echo ""
+    echo "=> Confirmando si se instalo correctamente Homebrew"
+
+    sleep 0
+
+    if ! command -v brew &> /dev/null; then
+        echo ""
+        echo "✦ La instalación presento un error"
+        exit 1
+    fi 
+
+    echo ""
+    echo "✦ Homebrew se instalo correctamente"
+fi
