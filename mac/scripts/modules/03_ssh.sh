eb7785039b3cd44dd2d89a2b62edacb7fac6908a
jane <183669693+Sikzyo@users.noreply.github.com>
2026-06-22 16:24:28 -0500

    chore: add ssh setup script

---
+++ mac/scripts/modules/03_ssh.sh
@@ -0,0 +1,134 @@
+GIT_EMAIL=$(git config --global user.email)
+
+echo "✦ Modulo SSH ✦"
+set -e
+
+echo ""
+echo "=> Validando si ya tienes creada una clave SSH"
+
+sleep 1
+
+if [ ! -f "$HOME/.ssh/id_ed25519.pub" ]; then
+    echo ""
+    echo "✦ Vamos a generar una nueva clave SSH"
+
+    sleep 1
+
+    if [ -z "$GIT_EMAIL" ]; then
+        echo ""
+        read -p "=> Ingresa un correo para generar la clave" new_email
+
+        sleep 1
+
+        GIT_EMAIL=$new_email
+    fi
+
+    echo ""
+    echo "=> Generando nueva clave"
+
+    ssh-keygen -t ed25519 -C "$GIT_EMAIL"
+
+    sleep 1
+
+    echo ""
+    echo "=> Iniciando agente SSH"
+
+    eval "$(ssh-agent -s)"
+
+    sleep 1
+
+    echo ""
+    echo "=> Generando configuración para GitHub"
+
+    if [ ! -f "$HOME/.ssh/config" ]; then
+        mkdir -p "$HOME/.ssh"
+        touch "$HOME/.ssh/config"
+    fi
+
+    if ! grep -q "Host github.com" "$HOME/.ssh/config"; then
+        echo "" >> "$HOME/.ssh/config"
+        echo "Host github.com" >> "$HOME/.ssh/config"
+        echo "  User git" >> "$HOME/.ssh/config"
+        echo "  AddKeysToAgent yes" >> "$HOME/.ssh/config"
+        echo "  UseKeychain yes" >> "$HOME/.ssh/config"
+        echo "  IdentityFile ~/.ssh/id_ed25519" >> "$HOME/.ssh/config"
+    fi
+
+    sleep 1
+
+    echo ""
+    echo "=> Agregando clave SSH al ssh-agent"
+
+    ssh-add --apple-use-keychain "$HOME/.ssh/id_ed25519"
+
+    sleep 1
+
+    echo ""
+    echo "✦ Vamos a agregar la clave ssh a GitHub"
+
+    sleep 1
+
+    echo ""
+    echo "=> Copiando clave publica..."
+
+    pbcopy < "$HOME/.ssh/id_ed25519.pub"
+
+    sleep 1
+
+    echo ""
+    echo "=> Llave copiada exitosamente"
+
+    echo ""
+    echo "✦ Agrega la clave que se acaba de copiar en GitHub"
+    echo "✦ Haz clic en New SSH key o Add SSH key."
+    echo "✦ En el campo Title, agrega un nombre descriptivo."
+    echo "✦ En el campo Key, pega tu clave pública (ya esta copiada en el portapapeles)."
+
+    sleep 5
+
+    while true; do
+        echo ""
+        read -p "¿Deseas abrir la GitHub? [y/n]: " open_web
+
+        sleep 1
+
+        case "$open_web" in
+            [yY])
+                echo ""
+                echo "=> Abriendo GitHub..."
+
+                sleep 1
+
+                open "https://github.com/settings/keys"
+                break
+            ;;
+            [nN])
+                echo ""
+                echo "✦ Recuerda que es super importante agregar la clave para poder continuar"
+
+                sleep 1
+                break
+            ;;
+            *)
+                echo ""
+                echo "=> Opción no valida, por favor presiona 'y' o 'n'"
+            ;;
+        esac
+    done
+
+    echo ""
+    read -p "✦ Una vez que hayas agregado la clave regresa y presiona cualquier tecla para continuar"
+
+    sleep 1
+fi
+
+echo ""
+echo "✦ Validando conexión con GitHub"
+
+sleep 1
+
+echo ""
+ssh -T git@github.com || true
+
+echo ""
+echo "✦ Creación de clave SSH y conexión a GitHub se realizo correctamente."
