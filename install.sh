5f16a9006af6af5a4b5626c8629eabedab1c4044
jane <183669693+Sikzyo@users.noreply.github.com>
2026-07-18 16:47:59 -0500

    fix(mac): mejorar la validación de instalación de Xcode y limpiar el código en install.sh; agregar archivos de configuración para Neovim

--- install.sh
+++ install.sh
@@ -4,91 +4,91 @@ USER_OS="Indefinido"
 DOTFILE_DIR="$HOME/.4k1"
 
 case $OSTYPE in
-    "darwin"*)
-        USER_OS="Mac"
-    ;;
-    "linux"*)
-        USER_OS="Linux"
-    ;;
-    *)
-        USER_OS="Sistema no soportado"
-    ;;
+"darwin"*)
+  USER_OS="Mac"
+  ;;
+"linux"*)
+  USER_OS="Linux"
+  ;;
+*)
+  USER_OS="Sistema no soportado"
+  ;;
 esac
 
 xcode_install() {
-    echo "✦ Vamos a validar si tienes instalado Xcode"
+  echo "✦ Vamos a validar si tienes instalado Xcode"
+
+  echo ""
+  if ! xcode-select -p &>/dev/null; then
+    echo "=> Instalando xcode"
 
     echo ""
-    if ! xcode-select -p &> /dev/null; then
-        echo "=> Instalando xcode"
+    echo "✦ Se ha abierto una ventana del sistema macOS"
+    echo "✦ Ve a esa ventana y completa la instalación"
+    echo "✦ Cuando la instalación haya terminado, regresa aquí"
 
-        echo ""
-        echo "✦ Se ha abierto una ventana del sistema macOS"
-        echo "✦ Ve a esa ventana y completa la instalación"
-        echo "✦ Cuando la instalación haya terminado, regresa aquí"
+    xcode-select --install
 
-        xcode-select --install
+    echo ""
+    while pgrep -f "Install Command Line Developer Tools" &>/dev/null; do
+      sleep 5
+    done
 
-        echo ""
-        while pgrep -f "Install Command Line Developer Tools" &> /dev/null; do
-            sleep 5
-        done
-
-        if xcode-select -p &> /dev/null; then
-            echo "✦ ¡Linea de comandos instalada correctamente!"
-        else
-            echo "✦ La instalación de las herramientas fallo"
-            echo "✦ No se puede continuar con la instalación"
-            exit 1
-        fi
+    if xcode-select -p &>/dev/null; then
+      echo "✦ ¡Linea de comandos instalada correctamente!"
     else
-        echo "✦ La linea de comandos para Xcode ya se encuentra instalada"
+      echo "✦ La instalación de las herramientas fallo"
+      echo "✦ No se puede continuar con la instalación"
+      exit 1
     fi
+  else
+    echo "✦ La linea de comandos para Xcode ya se encuentra instalada"
+  fi
 }
 
 mac_installer() {
-    xcode_install
+  xcode_install
 
-    if [ -d "$DOTFILE_DIR" ]; then
-        echo ""
-        echo "=> Limpiando instalaciones previas"
-
-        rm -rf "$DOTFILE_DIR"
-    fi
-
+  if [ -d "$DOTFILE_DIR" ]; then
     echo ""
-    echo "=> Creando carpeta"
+    echo "=> Limpiando instalaciones previas"
 
-    mkdir -p "$HOME/.4k1"
+    rm -rf "$DOTFILE_DIR"
+  fi
 
-    echo ""
-    echo "=> Clonando repositorio de GitHub"
+  echo ""
+  echo "=> Creando carpeta"
 
-    echo ""
-    git clone https://github.com/Sikzyo/4k1.git "$DOTFILE_DIR"
+  mkdir -p "$HOME/.4k1"
 
-    cd "$DOTFILE_DIR"
+  echo ""
+  echo "=> Clonando repositorio de GitHub"
 
-    zsh mac/install.sh
+  echo ""
+  git clone https://github.com/Sikzyo/4k1.git "$DOTFILE_DIR"
+
+  # cd "$DOTFILE_DIR"
+
+  zsh mac/install.sh
 }
 
 finish_install() {
-    echo ""
-    echo "✦ Iniciando proceso de limpieza"
+  echo ""
+  echo "✦ Iniciando proceso de limpieza"
 
-    echo ""
-    echo "=> Limpiando carpeta de instalación"
+  echo ""
+  echo "=> Limpiando carpeta de instalación"
 
-    cd "$HOME"
+  cd "$HOME"
 
-    rm -rf "$DOTFILE_DIR"
+  rm -rf "$DOTFILE_DIR"
 
-    echo ""
-    echo "✦ Limpieza terminada"
+  echo ""
+  echo "✦ Limpieza terminada"
 
-    echo ""
-    echo "✦ Instalación terminada, gracias por utilizar 4k1"
-    exit 0
+  echo ""
+  echo "✦ Instalación terminada, gracias por utilizar 4k1"
+  exit 0
 }
 
 echo "✦ Preparando instalación"
@@ -106,19 +106,19 @@ echo "✦ Tu sistema operativo es: $USER_OS"
 
 echo ""
 case "$USER_OS" in
-    "Mac")
-        echo "=> Iniciando configuración básica..."
-        echo ""
-        mac_installer
-    ;;
-    "Linux")
-        echo "=> Actualmente estamos trabajando en el soporte para Linux, esperamos lanzarlo lo mas pronto posible"
-        exit 0
-    ;;
-    *)
-        echo "=> Actualmente no disponemos soporte para tu sistema operativo"
-        exit 0
-    ;;
+"Mac")
+  echo "=> Iniciando configuración básica..."
+  echo ""
+  mac_installer
+  ;;
+"Linux")
+  echo "=> Actualmente estamos trabajando en el soporte para Linux, esperamos lanzarlo lo mas pronto posible"
+  exit 0
+  ;;
+*)
+  echo "=> Actualmente no disponemos soporte para tu sistema operativo"
+  exit 0
+  ;;
 esac
 
 finish_install
