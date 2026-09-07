0a50da419b7ca26f6072cf9cd313a6062f270341
jane <183669693+Sikzyo@users.noreply.github.com>
2026-09-06 20:31:58 -0500

    feat(execute): agregar función para ejecutar comandos sin salida estandar

--- internal/execute/command.go
+++ internal/execute/command.go
@@ -13,3 +13,11 @@ func Command(name string, args ...string) error {
 
 	return cmd.Run()
 }
+
+func CommandNull(name string, args ...string) error {
+	cmd := exec.Command(name, args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+
+	return cmd.Run()
+}
