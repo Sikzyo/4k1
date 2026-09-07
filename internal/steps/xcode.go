5907c0d58763b4e3fe62bca6f4c21f140b26e377
jane <183669693+Sikzyo@users.noreply.github.com>
2026-09-06 20:35:31 -0500

    feat(steps): mejorar la instalación de Xcode y el manejo de errores

--- internal/steps/xcode.go
+++ internal/steps/xcode.go
@@ -2,6 +2,7 @@ package steps
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/sikzyo/4k1/internal/execute"
 )
@@ -10,15 +11,43 @@ func InstallXcode() error {
 	fmt.Println("✦ Xcode ✦")
 
 	fmt.Println("-> Validando versión de xcode")
-	err := execute.Command("xcode-select", "-v")
+	cmd := execute.Command("xcode-select", "-p")
 
-	if err == nil {
+	if cmd == nil {
 		fmt.Println("-> Xcode instalado correctamente")
 		return nil
 	}
 
 	fmt.Println("-> Instalando xcode")
-	err = execute.Command("xcode-select", "--install")
+	cmd = execute.Command("xcode-select", "--install")
 
-	return err
+	if cmd != nil {
+		fmt.Println("-> Error al instalar Xcode")
+		return cmd
+	}
+
+	fmt.Println("✦ Se abrió una ventana para la instalación de Xcode")
+	fmt.Println("✦ Continua en esa ventana el proceso de instalación")
+	fmt.Println("✦ Al terminar la instalación de Xcode, regresa para continuar con la configuración")
+
+	for true {
+		// Comando para validar si el proceso de instalación termino
+		cmd = execute.CommandNull("pgrep", "-f", "Install Command Line Developer Tools")
+		if cmd != nil {
+			break
+		}
+
+		time.Sleep(5 * time.Second)
+	}
+
+	cmd = execute.Command("xcode-select", "-p")
+
+	if cmd != nil {
+		fmt.Println("-> Ocurrió un error al momento de instalar xcode")
+		return cmd
+	}
+
+	fmt.Println("La instalación de Xcode se ejecuto de manera correcta")
+
+	return nil
 }
